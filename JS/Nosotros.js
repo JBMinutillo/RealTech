c02df4c67ab7c1f28b78c0e32a516bbd93ee90e1
Juanba <juanbautistam123@gmail.com>
2023-12-23 00:32:48 -0300

    tpi

---
+++ JS/Nosotros.js
@@ -0,0 +1,29 @@
+//menu(hamburguesa)
+const barsMenu = document.querySelector(".navbar_list");
+
+//overlay
+const overlay = document.querySelector(".overlay");
+
+//btn para abrir y cerrar el menu
+const menuBtn =  document.querySelector(".menu_label");
+
+    const closeOverlayClick = () => {
+        overlay.classList.remove("show_overlay");
+    }
+
+    const toggleMenu = () => {
+        barsMenu.classList.toggle("open_menu");
+      
+      if(cartMenu.classList.contains("open_cart")){
+        cartMenu.classList.remove("open_cart");
+        return;
+      };
+      overlay.classList.toggle("show_overlay");
+    };
+
+    const init = () => {
+        menuBtn.addEventListener("click", toggleMenu);
+        overlay.addEventListener("click", closeOverlayClick);
+    };
+
+    init();
